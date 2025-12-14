e184ea5ab1edfd4740a2c47dbbd074c6e1e0e3d7
mbroumsadja <mbroumsadjaa@gmai.com>
2025-12-15 00:33:38 +0100

    mise a jour

--- config/api.js
+++ config/api.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 import axios from "axios";
 dotenv.config()
 
-
 export const newsapi = new axios.create({
     baseURL:'https://newsapi.org/v2',
     headers:{Authorization:`Bearer process.env.API_KEY`}
