a19f4cd89aaeb6bc1c055c300de8a8b5ff7bde39
= <=>
2026-05-09 19:26:22 +0100

    initialisation

--- models/Admin.js
+++
@@ -1,9 +0,0 @@
-import sequelize from "../data/config.js";
-import { DataTypes } from "sequelize";
-
-const Admin = sequelize.define('Admin', {
-  nom: DataTypes.STRING,
-  mots_passe: DataTypes.STRING
-}, { tableName: 'admin', timestamps: false });
-
-export default Admin
